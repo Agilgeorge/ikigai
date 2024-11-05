1b0994af2065b401cf96f459fb4f90c09efbf2ad
Agilgeorge <111696090+Agilgeorge@users.noreply.github.com>
2024-11-05 22:53:52 +0530

    some pages created for finishing

---
+++ src/calender.jsx
@@ -0,0 +1,9 @@
+
+// dont judge da bois i just want to maintain streak
+import React from 'react'
+
+export const calender = () => {
+  return (
+    <div>calender</div>
+  )
+}
---
+++ src/health.jsx
@@ -0,0 +1,9 @@
+
+// dont judge da bois i just want to maintain streak
+import React from 'react'
+
+export const health = () => {
+  return (
+    <div>health</div>
+  )
+}
