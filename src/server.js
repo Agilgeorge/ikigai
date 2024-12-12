e63d37a5b3df056b374568c67c07d9279ca1153b
Agilgeorge <111696090+Agilgeorge@users.noreply.github.com>
2024-12-12 23:58:17 +0530

    shifted to mongo atlas

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const PORT = 5000;
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/ikigai', {
+mongoose.connect('mongodb+srv://agilavenger017:<db_password>@ikigai.qokro.mongodb.net/?retryWrites=true&w=majority&appName=ikigai', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
